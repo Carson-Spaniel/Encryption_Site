70ea0373040d51ffcd0ff3d074e7283f87db6c15
Carson Spaniel <carsonspaniel@gmail.com>
2024-04-24 15:48:18 -0500

    trying to fix encryption

--- Encryption_App/models.py
+++ Encryption_App/models.py
@@ -1,6 +1,8 @@
 from django.contrib.auth.models import User
 from django.db import models
 from Crypto.PublicKey import RSA
+from Crypto.Cipher import AES
+from Crypto.Hash import SHA256
 
 class UserProfile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
@@ -8,13 +10,22 @@ class UserProfile(models.Model):
     # Add fields for public key and private key
     public_key = models.TextField(blank=True, null=True)
     private_key = models.TextField(blank=True, null=True)
+    tag = models.TextField(blank=True, null=True)
+    nonce = models.TextField(blank=True, null=True)
 
     passwordsJson = models.TextField(blank=True, null=True)
 
     def save(self, *args, **kwargs):
-        password = self.user.password.split('$')[-1][0:31]
-        print(password)
-        print(len(password))
+        sha256 = SHA256.new()
+        sha256.update(self.user.password.split('$')[-1][0:32].encode('utf-8'))
+        aes_key = sha256.digest()
+        cipher = AES.new(aes_key, AES.MODE_OCB)
+        print(f'\n\n\n',self.passwordsJson, f'\n\n\n')
+        passwordsCiphertext, tag = cipher.encrypt_and_digest(self.passwordsJson)
+
+        self.passwordsJson = tag + passwordsCiphertext
+        self.nonce = cipher.nonce
+
         # Generate RSA key pair if new user and keys are not set
         if not self.pk and (not self.public_key or not self.private_key):
             pub_key, priv_key = self.generate_rsa_keypair()
@@ -26,8 +37,11 @@ class UserProfile(models.Model):
                 4. Save encrypted data to designated areas                
             '''
 
-            self.public_key = pub_key.decode()
-            self.private_key = priv_key.decode()
+            publicCiphertext, tag = cipher.encrypt_and_digest(pub_key.decode())
+            self.public_key = tag + publicCiphertext
+
+            privateCiphertext, tag = cipher.encrypt_and_digest(priv_key.decode())
+            self.private_key = tag + privateCiphertext
         super().save(*args, **kwargs)
 
     def generate_rsa_keypair(self, key_size=2048):
