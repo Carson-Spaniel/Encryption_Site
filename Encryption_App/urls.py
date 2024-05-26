d10caf06b977254ef96e41e6a34dc62e953c7da9
Carson_Spaniel <carsonspaniel@gmail.com>
2024-05-26 00:11:37 -0500

    adding delete all passwords function

--- Encryption_App/urls.py
+++ Encryption_App/urls.py
@@ -10,6 +10,7 @@ urlpatterns = [
     path('passwords/', views.passwords_page, name='passwords_page'),
     path('add-password/', views.add_password_page, name='add_password_page'),
     path('remove-password/', views.remove_password, name='remove_password'),
+    path('remove--all-passwords/', views.remove_all_passwords, name='remove_all_passwords'),
     path('generate-username/', views.generate_username, name='generate_username'),
     path('generate-password/', views.generate_password, name='generate_password'),
     path('generate_passphrase/', views.generate_passphrase, name='generate_passphrase'),
